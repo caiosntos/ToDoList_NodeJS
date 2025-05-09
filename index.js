c94515efbb05196b679e4e870661761f674986de
caiosntos <caiogiovanists@outlook.com>
2025-05-08 23:09:05 -0300

    feat:Adcionado botão de erro estilizado com SweetAlert

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
@@ -7,22 +8,32 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
-let tasks = ['Passear com o dog', 'Ir ao mercado', 'Comprar pão'];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+let tasks = [];
 
 app.get('/', (req, res) => {
     res.render('index', {taskslist:tasks});
 });
 
 app.get('/deletar/:id', (req, res) => {
-    tasks = tasks.filter(function(val, index){
-        if(index != req.params.id){
-            return val;
-        }
-    })
-    res.render('index', {taskslist:tasks});
+    const id = parseInt(req.params.id);
+    if(id >= 0 && id < tasks.length){
+        tasks.splice(id, 1);
+    }
+    res.redirect('/');
 });
 
-
+app.post('/', (req, res) => {
+    const tarefa = req.body.task?.trim();
+    if (tarefa) {
+        tasks.push(tarefa);
+    }
+    res.render('index', { taskslist: tasks });
+});
 
 app.listen(5000, () =>{
     console.log('Servidor rodando em http://localhost:5000');
